8f7454113b2bc0a81fafc4b5a6b6fe1aafe7fd9f
tapixhx <tapish818@gmail.com>
2019-09-01 11:28:17 +0530

    folder 2 done

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,12 +3,26 @@ import { Component } from '@angular/core';
 @Component({
     selector : 'app-server',
     templateUrl : './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 
 export class ServerComponent{
     serverid: number = 10;
     serverStatus: string = ' offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    }
+
     getserverStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'Online' ? 'green' : 'red';
     }
 }
